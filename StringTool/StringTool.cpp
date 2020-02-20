af18243ba91f0d788ef1ca50d75e23ef53cb1ee7
ljb200788 <54976028+ljb200788@users.noreply.github.com>
2020-02-20 16:05:58 +0800

    初始化

--- StringTool/StringTool.cpp
+++ StringTool/StringTool.cpp
@@ -2,10 +2,212 @@
 //
 
 #include "stdafx.h"
+#include "strcpp.h"
 
+void split_test() {
+	std::string input = "We are the champions";     // input string
+	std::vector<std::string> result;                // result vector
 
-int main()
-{
-    return 0;
+	result = strcpp::split(input, " ");             // result = ["We", "are", "the", "champions"]
+	result = strcpp::split(input, " a");            // result = ["We", "re the champions"]
+	result = strcpp::split(input, "ions");          // result = ["We are the champ"]
+
+	input = "a,b,c,d,e";
+	result = strcpp::split(input, ",");             // result = ["a", "b", "c", "d", "e"]
+
+	input = "a, b, c, d, e";
+	result = strcpp::split(input, ", ");            // result = ["a", "b", "c", "d", "e"]
+
+	input = "LOGGER::ERROR::Error Message!";
+	auto message = strcpp::split(input, "::")[2];   // message = "Error Message!"
+
+	auto length = strcpp::length(message);          // length = 14
+}
+
+void slice_test() {
+	std::string input = "Hello World";
+	// End index is optional and defaults to end of string
+	std::cout << strcpp::slice(input, 0) << std::endl;     // All but the first zero characters  - "Hello World"
+	std::cout << strcpp::slice(input, 0, 1) << std::endl;  // Just the first character           - "H"
+	std::cout << strcpp::slice(input, 3) << std::endl;     // All but the first three characters - "lo World"
+	std::cout << strcpp::slice(input, 0, 5) << std::endl;  // Just the first five characters     - "Hello"
+	std::cout << strcpp::slice(input, 3, 8) << std::endl;  // After third till eigth character   - "lo Wo"
+	std::cout << strcpp::slice(input, -3) << std::endl;    // Just the last three characters     - "rld"
+	std::cout << strcpp::slice(input, 0, -3) << std::endl; // All but the last three characters  - "Hello Wo"
+}
+
+void contains_test() {
+	std::string input = "ERROR::Houston, we have a problem!";
+
+	if (strcpp::contains(input, "ERROR")) {                               // containment check - case sensitive
+		std::cout << "Input string contains \"ERROR\"" << std::endl;        // check returns true and prints message
+	}
+
+	if (strcpp::contains(input, "error::", true)) {                       // containment check - ignore case
+		std::cout << "Input string contains \"error::\"" << std::endl;      // check returns true and prints message
+	}
+
+	if (strcpp::contains(input, "PROBLEM")) {                             // containment check - ignore case
+		std::cout << "Input string contains \"PROBLEM\"\n";
+	}
+	else {
+		std::cout << "Input string does not contain \"PROBLEM\"\n";         // check returns false and prints message
+	}
+}
+
+void starts_ends_test() {
+	std::string input = "What's up with the Quaithe?";
+
+	if (strcpp::starts_with(input, 'W')) {
+		std::cout << "Input string starts with 'W'\n";                     // check returns true and prints message
+	}
+
+	if (strcpp::starts_with(input, 'w', true)) {
+		std::cout << "Input string starts with 'w' - Case is ignored\n";   // check returns true and prints message
+	}
+
+	if (strcpp::ends_with(input, '.')) {                                 // check returns false
+		std::cout << "Input string ends with period\n";
+	}
+
+	if (strcpp::ends_with(input, '?')) {                                 // check returns true and prints message
+		std::cout << "Input string is a question\n";
+	}
+}
+
+void count_test() {
+	std::string input = "Hello World!";
+	std::cout << strcpp::count(input, "l") << std::endl;          // prints 3
+	std::cout << strcpp::count(input, "llo") << std::endl;        // prints 1
+	std::cout << strcpp::count(input, " W") << std::endl;         // prints 1
+	std::cout << strcpp::count(input, " wo") << std::endl;        // prints 0
+	std::cout << strcpp::count(input, " wo", true) << std::endl;  // prints 1 - ignores case
+
+	std::string them_spaces = "Count, the number of          spaces";
+	std::cout << strcpp::count(them_spaces, " ") << std::endl;    // prints 13
+}
+
+void repeat_test() {
+	std::string input = "Ha";
+	std::cout << strcpp::repeat(input, 5) << std::endl;           // "HaHaHaHaHa"
+	std::cout << strcpp::repeat(input, 5, " ") << std::endl;      // "Ha Ha Ha Ha Ha"
+
+	input = "Ch";
+	auto result = strcpp::repeat(
+		strcpp::repeat(input, 2, "ooo!"),
+		3, " ");
+	std::cout << result << std::endl;                             // "Chooo!Chooo! Chooo!Chooo! Chooo!Chooo!"
+}
+
+void case_change_test() {
+	std::string input = "this is some lowercase string";
+	std::cout << strcpp::upper(input) << std::endl;               // "THIS IS SOME LOWERCASE STRING"
+
+	input = "ACCIDENTALLY ENABLED CAPS LOCK!!!";
+	std::cout << strcpp::lower(input) << std::endl;               // "accidentally enabled caps lock!!!"
+}
+
+void replace_test() {
+	std::string input = "This is a test string.";
+	std::cout << strcpp::replace(input, "a test", "an example");    // "This is an example string."
+	std::cout << std::endl <<
+		strcpp::replace(input, "test", "") << std::endl;              // "This is a string"
+
+	input = "This is a a test string";                              // replace the first occurrence of the letter 'a'
+	std::cout << strcpp::replace(input, "a", "", 1) << std::endl;   // "This is a test string"
+
+	input = "Peter Piper picked a peck of pickled peppers.";
+	std::cout << strcpp::replace(input, "p", "T", 2) << std::endl;  // "Peter PiTer Ticked a peck of pickled peppers."
+
+	input = "Peter Piper picked a peck of pickled peppers.";        // Find and replace does not ignore case
+	std::cout << strcpp::replace(input, "P", "T", 2) << std::endl;  // "Teter Tiper picked a peck of pickled peppers."
+}
+
+void translation_test() {
+	std::string input = "1X444 2X3AB *^$RB (F(QP.";
+	std::map<std::string, std::string> translation_table = {
+		{ "1X444", "Appetite" },
+		{ "2X3AB", "comes" },
+		{ "*^$RB", "with" },
+		{ "(F(QP", "eating" }
+	};
+	std::string translated = strcpp::translate(input, translation_table);
+	std::cout << translated << std::endl;                          // "Appetite comes with eating."
+}
+
+void trim_test() {
+	std::string input = "   Hello World!   ";
+	std::cout << strcpp::trim(input) << std::endl;    // "Hello World!"
+	std::cout << strcpp::ltrim(input) << std::endl;   // "Hello World!   "
+	std::cout << strcpp::rtrim(input) << std::endl;   // "   Hello World!"
+}
+
+void join_test() {
+	std::string input = "This. is. not. how. sentences. work.";        // wow such a sentence
+	auto split_string = strcpp::split(input, ". ");                    // split using period
+	std::string joined_string = strcpp::join(split_string, " ");       // join result of split
+	std::string output = strcpp::replace(joined_string, " not", "");   // remove the word 'not'
+	std::cout << output << std::endl;                                  // "This is how sentences work."
+
+	input = "Transaction_date, Product, Price, State";                 // some csv data
+	split_string = strcpp::split(input, ", ");                         // split using comma
+	output = strcpp::join(split_string, "::");                         // join result of split
+	std::cout << output << std::endl;                                  // "Transaction_date::Product::Price::State"
+}
+
+void find_test() {
+	std::string input = "I scream, you scream, we all scream for icecream!";
+	size_t find_index = strcpp::find(input, "you");                            // Index = 10
+	std::cout << find_index << std::endl;
+
+	find_index = strcpp::find_first(input, "cream");                           // Index = 3
+	std::cout << find_index << std::endl;
+
+	find_index = strcpp::find_last(input, "cream");                            // Index = 47
+	std::cout << find_index << std::endl;
+}
+
+void find_regex_test() {
+	std::string log(R"(
+        Speed : 366
+        Mass  :	 35
+        Speed : 378
+        Mass  :	 32
+        Speed : 400
+	      Mass  :	 30)");
+	std::string regex_string = R"(Speed : \d*)";
+	auto results = strcpp::find_regex(log, regex_string);
+	// ["Speed : 366", "Speed : 378", "Speed : 400"]
+
+	std::string file_path = "/home/pranav/dev/FILE_SomeStringOfInterest_EVENT.bash";
+	results = strcpp::find_regex(file_path, ".*FILE_(\\w+)_EVENT\\.bash.*");
+	// results: ["/home/pranav/dev/FILE_SomeStringOfInterest_EVENT.bash", "SomeStringOfInterest"]
+}
+
+void format_test() {
+	std::string output = strcpp::format("Roses are {0}, Violets are {1}, Sugar is {2}, And so are {3}!",
+	{ "red", "blue", "sweet", "you" });
+	std::cout << output << std::endl; // "Roses are red, Violets are blue, Sugar is sweet, And so are you!"
+
+	std::cout << strcpp::format("{0}!... {0}!", { "Hodor" }) << std::endl; // "Hodor!... Hodor!"
+}
+
+int main() {
+	split_test();
+	slice_test();
+	contains_test();
+	starts_ends_test();
+	count_test();
+	repeat_test();
+	case_change_test();
+	replace_test();
+	translation_test();
+	trim_test();
+	join_test();
+	find_test();
+	find_regex_test();
+	format_test();
+	system("PAUSE");
+	return 0;
 }
 
---
+++ StringTool/strcpp.h
@@ -0,0 +1,280 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cctype>
+#include <regex>
+
+namespace strcpp {
+
+  // returns length of input string
+  size_t length(const std::string input) {
+    return input.size();
+  }
+
+  // returns true/false if input string contains search string
+  bool contains(std::string input, std::string search_string, bool ignore_case = false) {
+    bool result = false;
+    if (ignore_case) { // if case is to be ignored, convert both input and search string to all upper case
+      std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+      std::transform(search_string.begin(), search_string.end(), search_string.begin(), ::toupper);
+    }
+    if (input.find(search_string) != std::string::npos) { // search using find algorithm
+      result = true;
+    }
+    return result;
+  }
+
+  // returns a sliced string
+  std::string slice(std::string input, int start_index, int end_index = std::string::npos) {
+    std::string result;
+    if (end_index == std::string::npos) { // if end index is default, i.e., till end of string
+      if (start_index < 0) { // if start index is negative, then start start_index characters from end of string
+        for (int i = static_cast<int>(input.size() + start_index); i < input.size(); i++) {
+          result += input[i];
+        }
+      }
+      else { // start_index is not negative; iterate from start_index till end of string
+        for (int i = static_cast<int>(start_index); i < input.size(); i++) {
+          result += input[i];
+        }
+      }
+    }
+    else { // end_index is not std::string::npos
+      if (end_index < 0) { // if end_index is negative, start from start_index and go till (end_of_string + end_index)
+        for (int i = static_cast<int>(start_index); i < input.size() + end_index; i++) {
+          result += input[i];
+        }
+      }
+      else { // if end_index is not negative either, then this is the trivial case
+        for (int i = start_index; i < end_index; i++) {
+          result += input[i];
+        }
+      }
+    }
+    return result;
+  }
+
+  // split string based on a delimiter string
+  // supports multi-character delimiter
+  // returns a vector of substrings after split
+  std::vector<std::string> split(std::string input, std::string delimiter, 
+    std::vector<std::string>& result = std::vector<std::string>()) {
+    size_t counter_position = 0;
+    size_t delimiter_position = input.find(delimiter); // checkf if delimiter is in input string
+    if (delimiter_position != std::string::npos) { // if delimiter position is not end_of_string
+      std::string split_string = input.substr(counter_position, delimiter_position);
+      delimiter_position += delimiter.size();
+      std::string split_remaining = input.erase(counter_position, delimiter_position);
+      result.push_back(split_string);
+      // Recursion! Recursion everywhere!
+      std::vector<std::string> result_remaining = split(split_remaining, delimiter, result);
+    }
+    else { // delimiter not in input string. Just add entire input string to result vector
+      result.push_back(input);
+    }
+    return result;
+  }
+
+  // checks if a string start_with input character
+  bool starts_with(std::string input, char c, bool ignore_case = false) {
+    bool result = false;
+    if (input.size() > 0) {
+      char first_character = input[0];
+      if (ignore_case) {
+        first_character = toupper(first_character);
+        c = toupper(c);
+      }
+      if (first_character == c)
+        result = true;
+    }
+    return result;
+  }
+
+  // checks if a string ends_with input character
+  bool ends_with(std::string input, char c, bool ignore_case = false) {
+    bool result = false;
+    if (input.size() > 0) {
+      char last_character = input[input.size() - 1];
+      if (ignore_case) {
+        last_character = toupper(last_character);
+        c = toupper(c);
+      }
+      if (last_character == c)
+        result = true;
+    }
+    return result;
+  }
+
+  // count number of times a substring appears in input string
+  size_t count(std::string input, std::string search_string, bool ignore_case = false) {
+    size_t result = 0;
+    if (contains(input, search_string, ignore_case)) {
+      if (ignore_case) {
+        std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+        std::transform(search_string.begin(), search_string.end(), search_string.begin(), ::toupper);
+      }
+      auto split_vector = split(input, search_string);
+      result = split_vector.size() - 1;
+    }
+    return result;
+  }
+
+  // repeat input string for count number of times, optionally with a connector string
+  std::string repeat(std::string input, int count, std::string connector = "") {
+    std::string result = "";
+    while (count > 0) {
+      result += input + connector;
+      count--;
+    }
+    return result;
+  }
+
+  // returns copy of input string that is all upper case
+  std::string upper(std::string input) {
+    std::string result;
+    result.resize(input.size());
+    std::transform(input.begin(), input.end(), result.begin(), ::toupper);
+    return result;
+  }
+
+  // returns copy of input string that is all lower case
+  std::string lower(std::string input) {
+    std::string result;
+    result.resize(input.size());
+    std::transform(input.begin(), input.end(), result.begin(), ::tolower);
+    return result;
+  }
+
+  // find and replace substring in input string, optionally for a limited number of times
+  // by default, every occurrence of find_string is replaced by replace_string
+  std::string replace(std::string input, std::string find_string, 
+    std::string replace_string, int replace_count = -1) {
+    std::string result = "";
+    auto split_vector = split(input, find_string);
+    auto split_size = split_vector.size();
+    auto max_replace_count = split_size - 1;
+    if (replace_count == -1) {
+      for (auto& s : split_vector) {
+        if (max_replace_count > 0)
+          result += s + replace_string;
+        else
+          result += s;
+        max_replace_count--;
+      }
+    }
+    else {
+      for (auto& s : split_vector) {
+        if (replace_count > 0) {
+          if (max_replace_count > 0)
+            result += s + replace_string;
+          else
+            result += s;
+          max_replace_count--;
+        }
+        else {
+          if (max_replace_count > 0)
+            result += s + find_string;
+          else
+            result += s;
+          max_replace_count--;
+        }
+        replace_count--;
+      }
+    }
+    return result;
+  }
+
+  // bulk find and replace sub-string in input string using translation table
+  std::string translate(std::string input, std::map<std::string, std::string> translation_table) {
+    std::string result = input;
+    for (auto& pair : translation_table) {
+      result = replace(result, pair.first, pair.second);
+    }
+    return result;
+  }
+
+  // trim white spaces from the left end of an input string
+  std::string ltrim(std::string input) {
+    std::string result = input;
+    result.erase(result.begin(), std::find_if(result.begin(), result.end(), [](int ch) {
+      return !std::isspace(ch);
+    }));
+    return result;
+  }
+
+  // trim white spaces from right end of an input string
+  std::string rtrim(std::string input) {
+    std::string result = input;
+    result.erase(std::find_if(result.rbegin(), result.rend(), [](int ch) {
+      return !std::isspace(ch);
+    }).base(), result.end());
+    return result;
+  }
+
+  // trim white spaces from either end of an input string
+  std::string trim(std::string input) {
+    return ltrim(rtrim(input));
+  }
+
+  // join a vector of strings into a single string
+  std::string join(std::vector<std::string> input, std::string connector = "") {
+    std::string result;
+    size_t max_connector_count = input.size() - 1;
+    for (auto& s : input) {
+      if (max_connector_count > 0)
+        result += s + connector;
+      else
+        result += s;
+      max_connector_count--;
+    }
+    return result;
+  }
+
+  // Returns true if the two input strings are equal
+  bool equal(std::string lhs, std::string rhs) {
+    return  (lhs.compare(rhs) == 0);
+  }
+
+  // generic find function
+  // if sub-string is found, returns the index
+  // if sub-string is not found, returns std::string::npos
+  size_t find(std::string input, std::string search_string) {
+    return input.find(search_string);
+  }
+
+  // finds first occurrence of sub-string in input string
+  size_t find_first(std::string input, std::string search_string) {
+    return input.find_first_of(search_string);
+  }
+
+  // finds last occurrence of sub-string in input string
+  size_t find_last(std::string input, std::string search_string) {
+    return input.find_last_of(search_string);
+  }
+
+  // performs std regex search and returns a vector of matched results
+  std::vector<std::string> find_regex(std::string input, std::string regex_string) {
+    std::vector<std::string> results;
+    std::smatch match_result;
+    while (std::regex_search(input, match_result, std::regex(regex_string))) {
+      for (auto& m : match_result)
+        results.push_back(m);
+      input = match_result.suffix();
+    }
+    return results;
+  }
+
+  // returns a formatted string for given the format and a vector of inputs
+  std::string format(std::string format, std::vector<std::string> inputs) {
+    std::string result = format;
+    for (size_t i = 0; i < inputs.size(); i++) {
+      std::string find = "{" + std::to_string(i) + "}";
+      result = replace(result, find, inputs[i]);
+    }
+    return result;
+  }
+
+}
